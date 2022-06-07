7cc2fc9aba2395efdc9e4b5627352a2df658c809
franco <francomiret11@gmail.com>
2022-06-07 00:44:41 -0300

    refactor

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,7 @@ export default {
   router: {
     base: '/plan-de-estudio/'
   },
+  
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: "%s - Plan de estudios",
